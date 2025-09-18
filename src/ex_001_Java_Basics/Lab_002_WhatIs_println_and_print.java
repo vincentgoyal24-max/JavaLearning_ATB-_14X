4709a8c6b7770a93bfa46276acc5e441bae016ed
vincentgoyal24-max <vincentgoyal24@gmail.com>
2025-09-18 23:01:19 +0530

    Lab002 and 003 commit and push

---
+++ src/ex_001_Java_Basics/Lab_002_WhatIs_println_and_print.java
@@ -0,0 +1,11 @@
+package ex_001_Java_Basics;
+
+public class Lab_002_WhatIs_println_and_print {
+  public static void main(String[] args){
+    System.out.println("println - Meaning of println is to print and then go to the next line");
+    System.out.print("print - Meaning of print is to print and then stay on the same line");
+    System.out.println("Hello world");
+    System.out.print("Hello print. ");
+    System.out.print("Second line but starting with same line as it is print command");
+  }
+}
---
+++ src/ex_001_Java_Basics/Lab_003_ProgramUsing_Notepad.java
@@ -0,0 +1,5 @@
+package ex_001_Java_Basics;
+
+public class Lab_003_ProgramUsing_Notepad {
+  public static void main(String[] args){
+}
