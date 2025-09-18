45673f49d8d948ce7a694bc3dfb06220e05d6fe7
vincentgoyal24-max <vincentgoyal24@gmail.com>
2025-09-18 23:17:25 +0530

    Code Issue using Notepad Lab003 commit and push

--- src/ex_001_Java_Basics/Lab_003_ProgramUsing_Notepad.java
+++ src/ex_001_Java_Basics/Lab_003_ProgramUsing_Notepad.java
@@ -2,4 +2,6 @@ package ex_001_Java_Basics;
 
 public class Lab_003_ProgramUsing_Notepad {
   public static void main(String[] args){
+    System.out.println("Not able to run the program using notepad, discus with team.");
+  }
 }
