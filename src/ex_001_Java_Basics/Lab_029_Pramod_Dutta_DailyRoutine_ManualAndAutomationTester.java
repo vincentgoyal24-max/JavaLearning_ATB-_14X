eabb1179c3af29babc94845e51c976e47b0fc413
vincentgoyal24-max <vincentgoyal24@gmail.com>
2025-09-24 12:44:24 +0530

    Pramod Dutta Answer - Daily Routine of automation and manual tester.

---
+++ src/ex_001_Java_Basics/Lab_029_Pramod_Dutta_DailyRoutine_ManualAndAutomationTester.java
@@ -0,0 +1,24 @@
+package ex_001_Java_Basics;
+
+public class Lab_029_Pramod_Dutta_DailyRoutine_ManualAndAutomationTester {
+  public static void main(String[] args) {
+    System.out.println("#  Daily Routine of Automation Tester (+ Manual Discuss) \n" +
+                       "- [ ] Syncs (Sprint Scrum) -_** Daily Stand ups**_ (Scrum Standups) - [\uFEFFhttps://sdet.live/agile](https://sdet.live/agile) \n" +
+                       "    1. What we have worked on last day\n" + "    2. What we are working on today\n" +
+                       "    3. Any Blockers\n" + "\n" + "- [ ] Env\n" +
+                       "    1. Sprints (Agile Env) - [\uFEFFsdet.live/agile](https://sdet.live/agile) \n" +
+                       "        1. Sprints Planning\n" + "        2. Retro Meeting\n" +
+                       "        3. Code Review, Peer Repo review.\n" + "        4. Backlog Refinement \n" + "\n" +
+                       "    2. Non Sprint\n" + "        1. Gather Requirement, Analysis)\n" +
+                       "        2. Test Cases ( Creation, Review ,Google Sheet, Zephyr, Xray, Practitest\n" +
+                       "        3. Test Plan and execution \n" + "\n" + "\n" +
+                       "1. Raise Bugs, Meetings, Internal demo, Defeat Management\n" +
+                       "2. Back log Refinements(Automation Tester)\n" +
+                       "    1. Backlog Automation ( 4-10 Test Cases Daily) \n" + "    2. Regression Maintenance\n" +
+                       "\n" + "3. Hotfixes Validation\n" + "    1. Automation Running (over the jenkins)\n" +
+                       "    2. Manual Testing \n" + "\n" + "4. Env Setup, Env Stability Check ( AT)\n" +
+                       "5. Collaboration within the Dev, Devops, PM Teams for better Test Coverage\n" +
+                       "6. Production Bugs Handling, Meetings, RCA, Fixing, Hotfixes, Release Validations\n" +
+                       "7. Framework Automation improvements (Activities)\n");
+  }
+}
